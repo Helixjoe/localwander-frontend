0059e1fdeb4b5e1d14c325a905379c1c8106e8e1
Helixjoe <joeljohn5112@gmail.com>
2024-07-13 17:46:07 +0530

    backend URL updated

--- src/pages/axiosConfig.js
+++ src/pages/axiosConfig.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000', // Your backend URL
+  baseURL: "https://localwander-backend.onrender.com", // Your backend URL
   withCredentials: true, // Include cookies in requests
 });
 
